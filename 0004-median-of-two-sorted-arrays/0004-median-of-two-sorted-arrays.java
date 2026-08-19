c7fff5f2cacdd8ec78af9c946075e2f33861c9de
Amit Singh <153449422+amitcsebtech@users.noreply.github.com>
2026-08-19 12:55:45 +0530

    Time: 1 ms (100%), Space: 48.8 MB (72.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,70 +1,45 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
 
-        // here in if condition i save our time that is avoid merge algo
-        if(n1 == 0){
-            if(n2%2 == 0){
-                return (nums2[n2/2] + nums2[n2/2-1])/2.0;
+        if(nums1.length > nums2.length){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+
+        int l1 = nums1.length;
+        int l2 = nums2.length;
+
+        int l = 0;
+        int h = l1;
+
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            int p1 = mid;
+            int p2 = (l1 + l2 +1)/2 - mid;
+
+            // left part
+            int x1 = (p1 == 0) ? Integer.MIN_VALUE:nums1[p1-1];
+            int x2 = (p2 == 0) ? Integer.MIN_VALUE:nums2[p2-1];
+
+            // right part
+            int x3 = (p1 == l1) ? Integer.MAX_VALUE:nums1[p1];
+            int x4 = (p2 == l2) ? Integer.MAX_VALUE:nums2[p2];
+
+            if(x1 <= x4 && x2 <= x3){
+                if((l1 + l2) % 2 == 1){
+                    return Math.max(x1,x2);
+                }
+
+                return (Math.max(x1,x2) + Math.min(x3,x4)) / 2.0;
+            }
+
+            if(x1 > x4){
+                h = mid-1;
             }
             else{
-                return nums2[n2/2];
+                l = mid+1;
             }
         }
 
-        if(n2 == 0){
-            if(n1%2 == 0){
-                return (nums1[n1/2] + nums1[n1/2-1])/2.0;
-            }
-            else{
-                return nums1[n1/2];
-            }
-        }
-
-        // i apply merge algo but not using extra space only maintain prev and curr value of array only because at the end i only need these two value
-
-        int mid = (n1+n2)/2;
-        int idx = 0;
-        int prev = -1;
-        int curr = -1;
-        int l1 = 0;
-        int l2 = 0;
-
-        // merging algorithm
-        while(l1 < n1 && l2 < n2 && idx <= mid){
-            if(nums1[l1] <= nums2[l2]){
-                prev = curr;
-                curr = nums1[l1];
-                l1++;
-            }
-            else{
-                prev = curr;
-                curr = nums2[l2];
-                l2++;
-            }
-            idx++;
-        }
-
-        // if first array contain some element but idx not reached to mid
-        while(l1 < n1  && idx <= mid){
-            prev = curr;
-            curr = nums1[l1];
-            l1++;
-            idx++;
-        }
-        // if second array contain some element but idx not reached to mid
-        while(l2 < n2 && idx <= mid){
-            prev = curr;
-            curr = nums2[l2];
-            l2++;
-            idx++;
-        }
-
-        // if size if odd then curr value point to mid 
-        if((n1+n2) % 2 != 0) return curr;
-
-        // if size is even then i need prev value also
-        return (prev + curr) / 2.0;
+        return -1;
     }
 }
